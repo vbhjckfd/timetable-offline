3dd82f4d62777abc34dd5d243145cfcf09ea62f1
Myroslav Holyak <vbhjckfd@gmail.com>
2024-07-01 15:12:28 +0300

    Fix local builds on mac laptops

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t gcr.io/timetable-252615/timetable-offline .
+docker build  --platform linux/amd64 -t gcr.io/timetable-252615/timetable-offline .
 docker push gcr.io/timetable-252615/timetable-offline
